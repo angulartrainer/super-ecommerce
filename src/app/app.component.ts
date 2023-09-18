206e8779143b842302e0e7c1b2875afc6a992b4c
Admin <admin@example.com>
2023-09-18 07:17:40 +0100

    Added changes for tailwind and top navigation component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopNavigationComponent } from './shared/top-navigation/top-navigation.component';
 
 @Component({
   selector: 'super-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TopNavigationComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'super-ecommerce';
---
+++ src/app/shared/top-navigation/top-navigation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'super-top-navigation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './top-navigation.component.html',
+  styleUrls: ['./top-navigation.component.scss'],
+})
+export class TopNavigationComponent {
+
+}
